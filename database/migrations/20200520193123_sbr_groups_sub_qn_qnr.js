3547a2833b86297d94b8b1a832d9a9b1003d8cf7
Giovanni <giovanni.carvalho@fundetec.org.br>
2020-05-28 22:03:48 -0300

    'hh'

--- database/migrations/20200520193123_sbr_groups_sub_qn_qnr.js
+++ database/migrations/20200520193123_sbr_groups_sub_qn_qnr.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('sbr_groups_sub_qn_qnr', (table)=>{
         table.increments('id');
-        table.integer('id_sbr_groups_sub', 11).unsigned().notNullable();
+        table.integer('id_sbr_groups_sub_qn', 11).unsigned().notNullable();
         table.integer('id_sbr_qnr', 11).unsigned().notNullable();
         table.timestamps(true, true);
         table.timestamp('deleted_at').nullable();
