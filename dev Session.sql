4be8135595cd92695fa81686215000922cbad398
Giovanni <gvnnfelicio@gmail.com>
2021-09-22 20:38:24 -0300

    Repository Change

--- dev Session.sql
+++
@@ -1,4 +0,0 @@
-insert into projects(user_id, title) values('2', 'Machine Learning'),('3', 'WebRTC'),('4', 'Broadcast RTC');
-select * from users;
-update users set created_at =  '2020-05-02 14:52:35' where id = 4;
-alter table projects auto_increment = 1;
